61aabf84951bab33af1e8413a933f5782881ec10
lautaroayosa <lautaroayosa@gmail.com>
2022-08-11 23:17:03 -0300

    Exercise 2.11 - db.json and axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Persons from './Components/Persons/Persons';
 import Filter from './Components/Filter/Filter';
 import PersonForm from './Components/PersonForm/PersonForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  },[])
 
   // Submit Handler
   function handleSubmit(event) {
