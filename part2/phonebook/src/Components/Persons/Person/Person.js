e64b24063f0d05e775f99b3a35416bed3717f8ec
lautaroayosa <lautaroayosa@gmail.com>
2022-08-13 23:02:10 -0300

    fix eslint warning

--- part2/phonebook/src/Components/Persons/Person/Person.js
+++ part2/phonebook/src/Components/Persons/Person/Person.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import phonebookService from '../../../services/phonebook'
 
 const Person = (props) => {
-
-    
 
     return (
         <li>{props.name} - {props.number} <button onClick={props.handleRemove} value={props.id} >Remove</button></li>
