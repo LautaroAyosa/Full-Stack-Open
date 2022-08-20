56932a7d7dc0d47d94c58fa8d3a5cabd2e9218d3
lautaroayosa <lautaroayosa@gmail.com>
2022-08-20 20:31:59 -0300

    Exercise 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,37 +1,36 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 app.use(express.json())
 
-var morgan = require('morgan')
-morgan.token("data", (request) => {
-    return request.method === "POST" ? JSON.stringify(request.body) : " ";
-  });
+const morgan = require('morgan')
+morgan.token('data', (request) => {
+  return request.method === 'POST' ? JSON.stringify(request.body) : ' '
+})
 app.use(
-morgan(":method :url :status :res[content-length] - :response-time ms :data")
-);
-
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
 
 let persons = [
-    {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-    },
-    {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-    },
-    {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-    },
-    {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-    }
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  }
 ]
 
 app.get('/', (req, res) => {
@@ -43,49 +42,50 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    let id = Number(req.params.id);
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).send(`Error! The ID wasn't valid or we couldn't find the person`)
-    }
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).send('Error! The ID wasn\'t valid or we couldn\'t find the person')
+  }
 })
 
 app.get('/info', (req, res) => {
-    var reqDate = new Date().toString();
-    res.send(`<p>Phonebook has infor for ${persons.length} people</p><p>${reqDate}</p>`)
-    res.status(404).send({ error: 'unknown endpoint' })
+  const reqDate = new Date().toString()
+  res.send(`<p>Phonebook has infor for ${persons.length} people</p><p>${reqDate}</p>`)
+  res.status(404).send({ error: 'unknown endpoint' })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    let id = Number(req.params.id);
-    const personsList = persons.filter(person => person.id !== id)
-    
-    response.status(204).send("DELETE req Called")
-    
+  const id = Number(req.params.id)
+  const personsList = persons.filter(person => person.id !== id)
+  res.send('DELETE req Called')
+  res.status(200).json(personsList)
 })
 
 app.post('/api/persons', (req, res) => {
-    const {name, number} = req.body;
-    id = Math.round(Math.random() * 60000);
+  const { name, number } = req.body
+  const id = Math.round(Math.random() * 60000)
 
+  if (name === undefined || number === undefined) {
+    res.status(404).send({ error: 'The Name or the Number is Undefined' })
+  } else if (persons.find(person => person.name === name)) {
+    res.status(404).send({ error: 'The person is already in the PhoneBook' })
+  } else {
     const newPerson = {
-        name: name,
-        number: number,
-        id: id
+      name,
+      number,
+      id
     }
 
     persons = [...persons, newPerson]
-    res.json({newPerson})
+    res.json({ newPerson })
+  }
 })
-
-
-
-
 
 const PORT = 3001
 app.listen(PORT, (err) => {
-    if (err) console.log(err)
-    console.log(`Server running on port ${PORT}`)
+  if (err) console.log(err)
+  console.log(`Server running on port ${PORT}`)
 })
