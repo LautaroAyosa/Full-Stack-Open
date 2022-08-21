3cc8787e85c621976b33d7778eeb8ce8319df019
lautaroayosa <lautaroayosa@gmail.com>
2022-08-21 01:34:39 -0300

    Exercises 3.13 - 3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -21,53 +21,75 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons)
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).end()
+    })
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (req, res) => {
+  const reqDate = new Date().toString()
+  Person
+    .find({})
+    .then(persons => {
+      res.send(`<p>Phonebook has infor for ${persons.length} people</p><p>${reqDate}</p>`)
+    })
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (body === undefined) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save().then(result => {
+    res.json(result)
   })
 })
 
-// app.get('/api/persons/:id', (req, res) => {
-//   const id = Number(req.params.id)
-//   const person = persons.find(person => person.id === id)
-//   if (person) {
-//     res.json(person)
-//   } else {
-//     res.status(404).send('Error! The ID wasn\'t valid or we couldn\'t find the person')
-//   }
-// })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
 
-// app.get('/info', (req, res) => {
-//   const reqDate = new Date().toString()
-//   res.send(`<p>Phonebook has infor for ${persons.length} people</p><p>${reqDate}</p>`)
-//   res.status(404).send({ error: 'unknown endpoint' })
-// })
-
-// app.delete('/api/persons/:id', (req, res) => {
-//   const id = Number(req.params.id)
-//   const personsList = persons.filter(person => person.id !== id)
-//   res.send('DELETE req Called')
-//   res.status(200).json(personsList)
-// })
-
-// app.post('/api/persons', (req, res) => {
-//   const { name, number } = req.body
-//   const id = Math.round(Math.random() * 60000)
-
-//   if (name === undefined || number === undefined) {
-//     res.status(404).send({ error: 'The Name or the Number is Undefined' })
-//   } else if (persons.find(person => person.name === name)) {
-//     res.status(404).send({ error: 'The person is already in the PhoneBook' })
-//   } else {
-//     const newPerson = {
-//       name,
-//       number,
-//       id
-//     }
-
-//     persons = [...persons, newPerson]
-//     res.json({ newPerson })
-//   }
-// })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+// Handler of errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, (err) => {
