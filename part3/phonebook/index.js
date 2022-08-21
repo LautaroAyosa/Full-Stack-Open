1ff500f1e597fc0ec57f71ebba345759c396f8ee
lautaroayosa <lautaroayosa@gmail.com>
2022-08-21 01:41:44 -0300

    Exercise 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -83,15 +83,15 @@ const unknownEndpoint = (request, response) => {
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
-// const errorHandler = (error, request, response, next) => {
-//   console.error(error.message)
-//   if (error.name === 'CastError') {
-//     return response.status(400).send({ error: 'malformatted id' })
-//   }
-//   next(error)
-// }
-// // Handler of errors
-// app.use(errorHandler)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+// Handler of errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, (err) => {
