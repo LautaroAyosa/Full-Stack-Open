75717a4ffc0615bd47cf01ffe359e55cf6254b80
lautaroayosa <lautaroayosa@gmail.com>
2022-08-21 00:04:41 -0300

    Exercise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://LautaroAyosa:${password}@cluster0.3o0bakd.mongodb.net/cluster0?retryWrites=true&w=majority`
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv[3]) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+    id: 1
+  })
+  person.save().then(result => {
+    console.log(`Added ${result.name} number ${result.number} to the Phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person
+    .find({})
+    .then(persons => {
+      console.log('Phonebook:')
+      persons.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+}
