a5cfc525b62cec66ac17c2da713d02b442ddb4c0
lautaroayosa <lautaroayosa@gmail.com>
2022-08-22 21:58:24 -0300

    Delete and put actions

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -6,6 +6,11 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
   if (blog.title && blog.url) {
@@ -20,6 +25,16 @@ blogsRouter.post('/', async (request, response) => {
   } else {
     response.status(400).send({ error: 'No Title or URL' })
   }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndDelete(request.params.id)
+  response.json(blog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndUpdate(request.params.id, { likes: request.body.likes })
+  response.send(`Blog '${blog.title}' Likes were changed from ${blog.likes} to ${request.body.likes}`)
 })
 
 module.exports = blogsRouter
