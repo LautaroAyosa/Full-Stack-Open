33924c908e5bafbc72e843aff235de6dba387734
lautaroayosa <lautaroayosa@gmail.com>
2022-09-04 21:24:08 -0300

    Exercises 6 and 7

--- part5/bloglist-frontend/src/components/Blogs/BlogsList/Blog/Blog.js
+++ part5/bloglist-frontend/src/components/Blogs/BlogsList/Blog/Blog.js
@@ -1,6 +1,21 @@
+import Togglable from "../../../Togglable/Togglable"
+
 const Blog = ({blog}) => (
-  <div>
-    <strong>{blog.title}</strong>, by {blog.author}
+  <div className="singleBlog">
+    <p className="singleBlogItem title"><strong>{blog.title}</strong> by {blog.author}</p>
+    <Togglable 
+      buttonLabel='Show' 
+      hideLabel='Hide' 
+      className='singleBlogTogglable'
+      showDivClassName='singleBlogShowButton'
+      contentDivClassName='singleBlogContent'
+    >
+      <p className="singleBlogItem url">{blog.url}</p>
+      <p className="singleBlogItem likes">
+        {blog.likes}
+        <button>Like</button>
+      </p>
+    </Togglable>
   </div>  
 )
 
